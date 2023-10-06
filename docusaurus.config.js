73d4cc70b29e9df308b4b8eacef89768e8437e54
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2023-10-06 11:42:10 +0200

    update social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -60,7 +60,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/renku-social-card.png",
       navbar: {
         title: "Renku Blog",
         logo: {
