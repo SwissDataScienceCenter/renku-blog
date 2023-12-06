04ceb1146ef61fcb86e91b2b24fd43941fae8697
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2023-12-06 15:24:50 +0100

    chore: fix navbar styling (#7)

---
+++ src/theme/Footer/LinkItem/index.js
@@ -0,0 +1,24 @@
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+
+// Original <FooterLinkItem> but with the <IconExternalLink> removed.
+export default function FooterLinkItem({ item }) {
+  const { to, href, label, prependBaseUrlToHref, ...props } = item;
+  const toUrl = useBaseUrl(to);
+  const normalizedHref = useBaseUrl(href, { forcePrependBaseUrl: true });
+  return (
+    <Link
+      className="footer__link-item"
+      {...(href
+        ? {
+            href: prependBaseUrlToHref ? normalizedHref : href,
+          }
+        : {
+            to: toUrl,
+          })}
+      {...props}
+    >
+      {label}
+    </Link>
+  );
+}
