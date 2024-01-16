e0e66c21e07f839fe1402858d5eff83bdd9bf690
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2024-01-16 15:56:50 +0100

    feat: update config for going to production

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,10 +12,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://swissdatasciencecenter.github.io",
+  url: "https://blog.renkulab.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/renku-blog/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
