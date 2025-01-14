6dbecd2e6e8dcf291ab2257d017264ff77805028
Flora Thiebaut <flora@leafty.dev>
2025-01-14 11:23:03 +0100

    feat: configure no trailing slash

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM nginxinc/nginx-unprivileged:1.25-alpine
 
+COPY nginx/default.conf /etc/nginx/conf.d/default.conf
+
 COPY build /usr/share/nginx/html
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,6 +17,8 @@ const config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
+  // Do not include a trailing slash for article pages.
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
